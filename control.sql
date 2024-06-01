f6fba1990c65b1dde0afc70b73ea2317902dc06d
Jeffrey Chen <jche0470@student.monash.edu>
2024-06-02 02:23:30 +1000

    fixed issue

--- control.sql
+++ control.sql
@@ -1,15 +1,4 @@
-set serveroutput off
-set echo off
-set feedback off
-set verify off
-
 @pf_initialSchemaInsert.sql
 @T1-pf-schema.sql
 @T2-pf-insert.sql
-
-set serveroutput on
-set echo on
-set feedback on
-set verify on
-
 @T3-pf-dml.sql
