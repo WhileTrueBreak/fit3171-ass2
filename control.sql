9faf2d949e7d4ebd076d2da150c581c0c1433c00
Jeffrey Chen <jche0470@student.monash.edu>
2024-06-02 02:08:28 +1000

    idk what fixed it

--- control.sql
+++ control.sql
@@ -1,5 +1,15 @@
-set serveroutput on
+set serveroutput off
+set echo off
+set feedback off
+set verify off
+
 @pf_initialSchemaInsert.sql
 @T1-pf-schema.sql
 @T2-pf-insert.sql
--- @T3-pf-dml.sql
+
+set serveroutput on
+set echo on
+set feedback on
+set verify on
+
+@T3-pf-dml.sql
