00958cae7fd7ceffa8f6ce9da4679eabb1950e05
Jeffrey Chen <jche0470@student.monash.edu>
2024-06-02 18:52:17 +1000

    t5 stuff

--- T5-pf-plsql.sql
+++ T5-pf-plsql.sql
@@ -13,23 +13,78 @@
 --(a)
 --Write your trigger statement,
 --finish it with a slash(/) followed by a blank line
-
+CREATE OR REPLACE TRIGGER TR_SERVICE_COST_IN_RANGE BEFORE
+    INSERT ON VISIT_SERVICE FOR EACH ROW
+DECLARE
+    VAR_SERVICE_STD_COST NUMBER;
+    VAR_LOWER_COST       NUMBER;
+    VAR_UPPER_COST       NUMBER;
+BEGIN
+    SELECT
+        S.SERVICE_STD_COST INTO VAR_SERVICE_STD_COST
+    FROM
+        SERVICE S
+    WHERE
+        S.SERVICE_CODE = :NEW.SERVICE_CODE;
+    VAR_LOWER_COST := VAR_SERVICE_STD_COST * 0.9;
+    VAR_UPPER_COST := VAR_SERVICE_STD_COST * 1.1;
+    IF (:NEW.VISIT_SERVICE_LINECOST < VAR_LOWER_COST
+    OR :NEW.VISIT_SERVICE_LINECOST > VAR_UPPER_COST) THEN
+        RAISE_APPLICATION_ERROR(-20000, 'Line cost must be within 10%');
+    END IF;
+END;
+/
 
 -- Write Test Harness for (a)
 -- initial data
 
 
+
 --(b)
 -- Complete the procedure below
-CREATE OR REPLACE PROCEDURE prc_followup_visit (
-    p_prevvisit_id IN NUMBER,
-    p_newvisit_datetime IN DATE,
-    p_newvisit_length IN NUMBER,
-    p_output OUT VARCHAR2
+CREATE OR REPLACE PROCEDURE PRC_FOLLOWUP_VISIT (
+    P_PREVVISIT_ID IN NUMBER,
+    P_NEWVISIT_DATETIME IN DATE,
+    P_NEWVISIT_LENGTH IN NUMBER,
+    P_OUTPUT OUT VARCHAR2
 ) IS
-    
+    VAR_PREV_ANIMAL NUMBER;
+    VAR_PREV_VET    NUMBER;
+    VAR_PREV_CLINIC NUMBER;
 BEGIN
-    
+    SELECT
+        V.ANIMAL_ID INTO VAR_PREV_ANIMAL
+    FROM
+        VISIT V
+    WHERE
+        V.VISIT_ID = P_PREVVISIT_ID;
+    SELECT
+        V.VET_ID INTO VAR_PREV_VET
+    FROM
+        VISIT V
+    WHERE
+        V.VISIT_ID = P_PREVVISIT_ID;
+    SELECT
+        V.CLINIC_ID INTO VAR_PREV_CLINIC
+    FROM
+        VISIT V
+    WHERE
+        V.VISIT_ID = P_PREVVISIT_ID;
+    INSERT INTO VISIT (
+        VISIT_ID,
+        VISIT_DATE_TIME,
+        VISIT_LENGTH,
+        ANIMAL_ID,
+        VET_ID,
+        CLINIC_ID
+    ) VALUES (
+        VISIT_PK_SEQ.NEXTVAL,
+        P_NEWVISIT_DATETIME,
+        P_NEWVISIT_LENGTH,
+        VAR_PREV_ANIMAL,
+        VAR_PREV_VET,
+        VAR_PREV_CLINIC
+    );
 END;
 /
 
--- control.sql
+++ control.sql
@@ -2,3 +2,4 @@
 @T1-pf-schema.sql
 @T2-pf-insert.sql
 @T3-pf-dml.sql
+@T4-pf-mods.sql
